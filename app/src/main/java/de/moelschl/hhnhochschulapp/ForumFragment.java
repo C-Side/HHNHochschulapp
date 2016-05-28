9902841edb4ab5466dec4a3a1d211535b51db497
Hasbert <fu@fu.com>
2016-05-29 01:48:26 +0200

    so aber jetzt gehts los

--- app/src/main/java/de/moelschl/hhnhochschulapp/ForumFragment.java
+++ app/src/main/java/de/moelschl/hhnhochschulapp/ForumFragment.java
@@ -1,19 +1,26 @@
 package de.moelschl.hhnhochschulapp;
 
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 /**
  * Created by moelscmar on 19.05.2016.
  */
-public class ForumFragment extends Fragment {
+public class ForumFragment extends ListFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        String[] values = new String[] { "Message1", "Message2", "Message3" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
 
         View rootView = inflater.inflate(R.layout.fragment_forum, container, false);
         return rootView;
