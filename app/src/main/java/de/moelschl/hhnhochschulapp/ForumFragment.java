ca3ad8482bfb8353ef667fe31a259877939fc6c2
Hasbert <fu@fu.com>
2016-05-29 02:54:23 +0200

    link to clicked item in the list view

--- app/src/main/java/de/moelschl/hhnhochschulapp/ForumFragment.java
+++ app/src/main/java/de/moelschl/hhnhochschulapp/ForumFragment.java
@@ -8,16 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by moelscmar on 19.05.2016.
  */
 public class ForumFragment extends ListFragment {
+    private String [] itemContent;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        String [] itemContent = getResources().getStringArray(R.array.topic);
+        this.itemContent = getResources().getStringArray(R.array.topic);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, itemContent);
 
@@ -25,6 +29,13 @@ public class ForumFragment extends ListFragment {
 
         View rootView = inflater.inflate(R.layout.fragment_forum, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        //View child = l.getChildAt(position);
+        String childname = itemContent[position];
+        Toast.makeText(getActivity(),childname, Toast.LENGTH_LONG).show();
     }
 }
 
