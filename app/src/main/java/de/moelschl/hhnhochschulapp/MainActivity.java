573ff44e24e6113f7f729cb74df462cc372f17fb
Marco Moelschl <mmoelsch@stud.hs-heilbronn.de>
2016-06-02 11:24:41 +0200

    Login Screen

--- app/src/main/java/de/moelschl/hhnhochschulapp/MainActivity.java
+++ app/src/main/java/de/moelschl/hhnhochschulapp/MainActivity.java
@@ -80,8 +80,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_logout) {
+           finish();
         }
 
         return super.onOptionsItemSelected(item);
@@ -259,4 +259,6 @@ public class MainActivity extends AppCompatActivity
         DrawableCompat.setTint(iliasImage.getDrawable(), ContextCompat.getColor(getApplicationContext(), R.color.black));
         setTitle("Tutorial");
     }
+
+
 }
