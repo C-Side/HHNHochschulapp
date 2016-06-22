5698a014e8bf71a2cb6cde57727fce9dc2564229
Hasbert <fu@fu.com>
2016-06-23 00:20:28 +0200

    activity steht fragment läuft darin

--- app/src/main/java/de/moelschl/hhnhochschulapp/controller/ForumActivity.java
+++ app/src/main/java/de/moelschl/hhnhochschulapp/controller/ForumActivity.java
@@ -3,15 +3,12 @@ package de.moelschl.hhnhochschulapp.controller;
 /**
  * Created by Hasbert on 21.06.2016.
  */
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.Window;
+
 
 import de.moelschl.hhnhochschulapp.R;
 
@@ -22,22 +19,18 @@ public class ForumActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_forum);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_forum);
         setSupportActionBar(toolbar);
 
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.forum_toolbar_fragment, new ToolbarFragment());
+        ft.add(R.id.forum_list_fragment, new ForumFragment());
+        ft.commit();
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-
-        android.app.FragmentManager fm = getFragmentManager();
-        fm.beginTransaction().replace(R.id.forum_fragment, new ForumFragment()).commit();
-
-
+        setTitle("Forum");
     }
 
 }
