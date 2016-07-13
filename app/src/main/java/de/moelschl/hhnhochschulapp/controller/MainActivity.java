a91bfebd96860504c463197cb8e25c797aea4fe4
Hasbert <fu@fu.com>
2016-07-13 15:58:17 +0200

    so jetzt hat das forum die ganzen navigationsitems. ich habs nicht geschafft mit der vererbung, weil das zum jetzigen standpunkt vom projekt bissle schwer is -.-

--- app/src/main/java/de/moelschl/hhnhochschulapp/controller/MainActivity.java
+++ app/src/main/java/de/moelschl/hhnhochschulapp/controller/MainActivity.java
@@ -1,9 +1,9 @@
 package de.moelschl.hhnhochschulapp.controller;
 
-
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,24 +18,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import de.moelschl.hhnhochschulapp.controller.Forum.FoActivity;
+import java.io.File;
+
 import de.moelschl.hhnhochschulapp.R;
+import de.moelschl.hhnhochschulapp.controller.Forum.FoCommentFragment;
+import de.moelschl.hhnhochschulapp.controller.Forum.FoQuestionAdder;
+import de.moelschl.hhnhochschulapp.controller.Forum.FoThemeFragment;
+import de.moelschl.hhnhochschulapp.controller.Forum.FoThreadFragment;
+import de.moelschl.hhnhochschulapp.io.DatabaseHelper;
+import de.moelschl.hhnhochschulapp.tools.CustomAutoCompleteView;
+
+/**
+ * the activity which holds the navigation-sets like appbar and toolbar and controls the action
+ * of the applied fragments. Is a controller for Forum Fragments.
+ *
+ */
+
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, FoThemeFragment.OnThemeSelectedListener,
+        FoThreadFragment.OnThemeSelectedListener, CustomAutoCompleteView.OnThemeChooseListener,
+        FoQuestionAdder.OnStoreData {
 
-    ImageView kalenderImage;
-    ImageView einstellungenImage;
-    ImageView homeImage;
-    ImageView nachrichtenImage;
-    ImageView iliasImage;
+    private DatabaseHelper mDBHelper;
+    private ImageView kalenderImage;
+    private ImageView einstellungenImage;
+    private ImageView homeImage;
+    private ImageView nachrichtenImage;
+    private ImageView iliasImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -49,16 +69,46 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        openHome();
+        setAllImageListeners();
+        //setAppbar();
+    }
+
+    /**
+     * switches the Fragment
+     *
+     * @param newFragment the new Fragment wich should be shown
+     */
+
+    private void switchFragment(Fragment newFragment){
         FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.content_frame, new HomeFragment());
+        ft.replace(R.id.content_frame, newFragment);
         ft.addToBackStack("tag");
         ft.commit();
-
-        setAllImageListeners();
-
-
     }
+
+    /**
+     * switches back to the last fragment when the back button is pressed
+     *
+     * this is the method of the old FoActivity
+
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 1) {
+            getFragmentManager().popBackStack();
+            popTitleStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+     */
+
+
+    /**
+     * switches back to the last fragment when the back button is pressed
+     */
 
     @Override
     public void onBackPressed() {
@@ -96,6 +146,7 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -126,24 +177,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-
-    /**
-     * switches the Fragment
-     *
-     * @param newFragment the new Fragment wich should be shown
-     */
-
-    private void switchFragment(Fragment newFragment){
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.content_frame, newFragment);
-        ft.addToBackStack("tag");
-        ft.commit();
-    }
-
     private void setAllImageListeners() {
-
-
         kalenderImage = (ImageView) findViewById(R.id.Kalender);
         einstellungenImage = (ImageView) findViewById(R.id.einstellungen);
         homeImage = (ImageView) findViewById(R.id.Home);
@@ -158,7 +192,6 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-
         homeImage.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -166,7 +199,6 @@ public class MainActivity extends AppCompatActivity
                 openHome();
             }
         });
-
 
         nachrichtenImage.setOnClickListener(new View.OnClickListener() {
 
@@ -176,7 +208,6 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-
         iliasImage.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -185,7 +216,6 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-
         einstellungenImage.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -193,27 +223,23 @@ public class MainActivity extends AppCompatActivity
                 openEinstellungen();
             }
         });
-
     }
 
     private void openKalender(){
         KalenderFragment kalenderFragment = new KalenderFragment();
         switchFragment(kalenderFragment);
-        setAppbar();
         setTitle("Kalender");
     }
 
     private void openHome(){
         HomeFragment homeFragment = new HomeFragment();
         switchFragment(homeFragment);
-        setAppbar();
         setTitle("HHN Hochschulapp");
     }
 
     private void openBenachrichtigungen(){
         BenachrichtigungenFragment benachrichtigungenFragment = new BenachrichtigungenFragment();
         switchFragment(benachrichtigungenFragment);
-        setAppbar();
         setTitle("Benachrichtigungen");
     }
 
@@ -226,7 +252,6 @@ public class MainActivity extends AppCompatActivity
     private void openEinstellungen(){
         EinstellungenFragment einstellungenFragment = new EinstellungenFragment();
         switchFragment(einstellungenFragment);
-        setAppbar();
         setTitle("Einstellungen");
     }
 
@@ -235,23 +260,11 @@ public class MainActivity extends AppCompatActivity
         startActivity(browserIntent);
     }
 
-
-    /**
-     * opens the Forum activity
-     *
-     */
-
-    private void openForum(){
-        startActivity(new Intent(MainActivity.this, FoActivity.class));
-    }
-
     private void openTutorial(){
         TutorialsFragment tutorialsFragment = new TutorialsFragment();
         switchFragment(tutorialsFragment);
-        setAppbar();
         setTitle("Tutorial");
     }
-
 
     /**
      * sets the icons of the app bar
@@ -260,9 +273,139 @@ public class MainActivity extends AppCompatActivity
 
     private void setAppbar(){
         DrawableCompat.setTint(einstellungenImage.getDrawable(), ContextCompat.getColor(getApplicationContext(), R.color.black));
-        DrawableCompat.setTint(kalenderImage.getDrawable(), ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary));
+        DrawableCompat.setTint(kalenderImage.getDrawable(), ContextCompat.getColor(getApplicationContext(), R.color.black));
         DrawableCompat.setTint(homeImage.getDrawable(), ContextCompat.getColor(getApplicationContext(), R.color.black));
         DrawableCompat.setTint(nachrichtenImage.getDrawable(), ContextCompat.getColor(getApplicationContext(), R.color.black));
         DrawableCompat.setTint(iliasImage.getDrawable(), ContextCompat.getColor(getApplicationContext(), R.color.black));
     }
+
+    //Forum activities will be written here, because we were not able to inheirite a base Toolbar
+    //in the actual version of our project. The Project is nearly finished and for such a big change
+    //was no time
+
+    /**
+     * opens the Forum activity
+     *
+     */
+
+    private void openForum(){
+        //vorher --> startActivity(new Intent(MainActivity.this, FoActivity.class));
+        initDatabase();
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.content_frame, new FoThemeFragment());
+        ft.addToBackStack("tag");
+        ft.commit();
+    }
+
+    /**
+     * a helper method to replace the current Frgament with a new Fragment.
+     *
+     * @param newFragment the next fragment
+     */
+
+    private void switchForumFragment(Fragment newFragment){
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.content_frame, newFragment);
+        ft.addToBackStack("tag");
+        ft.commit();
+    }
+
+    /**
+     * initialize the Database and prints out a text
+     *
+     */
+
+    private void initDatabase(){
+        mDBHelper = new DatabaseHelper(this);
+
+        //Check exists database
+        File database = getApplicationContext().getDatabasePath(DatabaseHelper.DBNAME);
+        if(false == database.exists()){
+            mDBHelper.getReadableDatabase();
+            //copy Db
+            if (mDBHelper.copyDatabase(this)){
+                Toast.makeText(this, "Copy database successful", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Copy error", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+    }
+
+
+
+    /**
+     *
+     * @param navigationKey
+     */
+
+    @Override
+    public void onThemeClicked(String navigationKey) {
+        FoThreadFragment threadFragment = new FoThreadFragment();
+        threadFragment.setNavigationKey(navigationKey);
+        switchForumFragment(threadFragment);
+        //addToTitleStack(firstToUpperCase(navigationKey));
+    }
+
+
+    /**
+     *
+     * @param navigationKey
+     * @param question
+     */
+
+    @Override
+    public void onThreadClicked(int navigationKey, String question, String questionHeader) {
+        FoCommentFragment commentFragment = new FoCommentFragment();
+        commentFragment.setNavigationKey(navigationKey);
+        commentFragment.setQuestion(question, questionHeader);
+        switchForumFragment(commentFragment);
+        //addToTitleStack("Kommentare");
+    }
+
+
+    /**
+     *
+     */
+
+    @Override
+    public void onNewQuestionClick() {
+        FoQuestionAdder questionFragment = new FoQuestionAdder();
+        switchForumFragment(questionFragment);
+    }
+
+
+    /**
+     *
+     * @param text
+     */
+
+    @Override
+    public void onDropDownItemClick(String text) {
+        EditText themeDesc = (EditText) findViewById(R.id.theme_desc_text);
+        String description = mDBHelper.getDescByTopic(text);
+        themeDesc.setText(description);
+    }
+
+    /**
+     *
+     *
+     * @param themeTopic
+     * @param themeDesc
+     * @param questionHeader
+     * @param question
+     * @param context
+     */
+
+    @Override
+    public void storeNewQuestion(String themeTopic, String themeDesc, String questionHeader, String question, Context context) {
+        mDBHelper.addQuestionToDatabase(themeTopic, themeDesc, questionHeader, question, context);
+        onBackPressed();
+    }
+
+
 }
