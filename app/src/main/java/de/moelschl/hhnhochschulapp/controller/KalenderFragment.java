7ddd68d83e78ea983f6b5c20a1f787a62d88b41a
Chippchen <anihenning@gmail.com>
2016-07-17 16:08:47 +0200

    Datum vom Termin angepasst

--- app/src/main/java/de/moelschl/hhnhochschulapp/controller/KalenderFragment.java
+++ app/src/main/java/de/moelschl/hhnhochschulapp/controller/KalenderFragment.java
@@ -41,7 +41,7 @@ public class KalenderFragment extends Fragment {
             @Override
             public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
                dateView.setText("Date: " + i2 + "." + i1 + "." + i);
-                if(dateView.getText().equals("Date: 18.7.2016")){
+                if(dateView.getText().equals("Date: 18.6.2016")){
                     dateView.setText("Interface Projects\nA212\n8:00 - 9:30");
 
                 }
